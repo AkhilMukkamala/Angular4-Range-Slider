eae95a259a42df0cf5aeb565a6b0146b2386a15a
Ram-SR <rammurthythati@gmail.com>
2017-11-14 04:03:59 -0800

    Added project files

---
+++ custom-range-slider/custom-range-slider.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit, ViewChild, ElementRef, HostListener, Input, Renderer } from '@angular/core';
+
+@Component({
+    selector: 'app-custom-range-slider',
+    templateUrl: './custom-range-slider.component.html',
+    styleUrls: ['./custom-range-slider.component.scss']
+})
+export class CustomRangeSliderComponent implements OnInit {
+    @ViewChild('sliderBar') sliderBar: ElementRef;
+    @Input() sliderConfig;
+    percentage: any;
+    thumb;
+    overlay;
+    dot;
+    filledBar;
+    initMouseMove;
+    
+    /**
+     * constructor function of the component 
+     */
+    constructor(private elemRef: ElementRef, private renderer: Renderer) { }
+
+    /**
+     * called on the component initialization
+     */
+    ngOnInit() {    
+
+        // store slider move function in a variable for same reference in add/remove listeners
+        this.initMouseMove = this.onMouseMoveOnSlider.bind(this);
+        
+        // calculate initial percentage
+        this.percentage = (this.sliderConfig.start / this.sliderConfig.max) * 100;
+
+        // get thumb, overlay, dot and bar
+        this.thumb = this.sliderBar.nativeElement.querySelector('.c2-thumb-value');
+        this.overlay = this.sliderBar.nativeElement.querySelector('.overlay');
+        this.dot = this.sliderBar.nativeElement.querySelector('.c2-js-slider-movable-thumb-dot');
+        this.filledBar = this.sliderBar.nativeElement.querySelector('.c2-filled-bar');
+
+        if(!this.sliderConfig.thumbShow) {
+            this.thumb.classList.add('hide-completely')
+        }
+
+        this.setSliderUIValues(this.percentage);
+    }
+
+	/**
+	 * check for mouse click and mouse move, set ui values of slider
+     * @param {Object} event - mousemove event object
+	 */
+    onMouseMoveOnSlider(event: MouseEvent) {
+
+        // show overlay
+        this.overlay.classList.remove('c2-hide');
+
+        // check for slider bar
+        if (this.sliderBar) {
+
+            // show thumb
+            this.thumb.classList.remove('c2-hide');
+
+            // calculate percentage according to mouse movement
+            let sliderLeftPos = this.sliderBar.nativeElement.offsetLeft;
+            let sliderRightPos = sliderLeftPos + this.sliderBar.nativeElement.offsetWidth;
+            let mouseLeft = event.pageX;
+            if (mouseLeft < sliderLeftPos) {
+                this.percentage = 0;
+            } else if (mouseLeft > sliderRightPos) {
+                this.percentage = 100;
+            } else {
+                this.percentage = ((event.pageX - sliderLeftPos) / this.sliderBar.nativeElement.offsetWidth) * 100;
+            }
+
+            this.setSliderUIValues(this.percentage);
+            
+            if(this.sliderConfig.selectCallBack) {
+                this.sliderConfig.selectCallBack(this.percentage);
+            }
+            return this.percentage; // return calculated/set percentage
+        }
+    }
+
+    /**
+     * add mouse move event on document
+     * @param {Object} event - mousedown event object
+     */
+    onMouseDown(event) {
+        document.addEventListener('mousemove', this.initMouseMove);
+    }
+
+    /**
+     * add mouse up event on window
+     * remove mouse move event on document
+     * @param {Object} event - mouseup event object
+     */
+    @HostListener('window:mouseup', ['$event'])
+    onMouseUp(event) {
+        this.thumb.classList.add('c2-hide');
+        this.overlay.classList.add('c2-hide');
+        document.removeEventListener('mousemove', this.initMouseMove);
+    }
+
+    /**
+     * move dot on click
+     * set ui values of slider
+     * @param event - click event object
+     */
+    onClick(event) {
+        this.thumb.classList.remove('c2-hide');
+        let sliderLeftPos = this.sliderBar.nativeElement.offsetLeft;
+        this.percentage = ((event.pageX - sliderLeftPos) / this.sliderBar.nativeElement.offsetWidth) * 100;
+        this.setSliderUIValues(this.percentage);
+        setTimeout(()=>{
+            this.thumb.classList.add('c2-hide');
+        }, 300);
+
+        if(this.sliderConfig.selectCallBack) {
+            this.sliderConfig.selectCallBack(this.percentage);
+        }
+        return this.percentage; // return calculated/set percentage
+    }
+
+    /**
+     * set filled bar value
+     * set dot position
+     * set thumb value
+     * @param { number } percentage - any number
+     */
+    setSliderUIValues(percentage) {
+        this.dot.style.left = percentage + "%";
+        this.filledBar.style.width = percentage + "%";
+        this.thumb.innerHTML = parseInt(percentage) + "%";
+    }
+}
